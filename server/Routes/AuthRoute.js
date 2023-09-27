1dc2318390a692aef96fb1dd9144d2c2c32636ef
rami jawadi <ramijawadi104@gmail.com>
2023-09-27 10:02:42 +0100

    FIX : cote backend with routes + cotè fronend consmmation des API

---
+++ server/Routes/AuthRoute.js
@@ -0,0 +1,11 @@
+
+const {  Login ,Signup } = require ('../Controllers/AuthController.js');
+const { userVerification } = require('../Middlewares/AuthMiddleware.js');
+
+const router = require('express').Router()
+
+router.post("/signup", Signup);
+router.post('/login', Login);
+router.post('/', userVerification);
+
+module.exports = router ;
